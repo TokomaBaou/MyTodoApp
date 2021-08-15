48ffde87d42257661ebba09554f73f7d679b5fdd
MAKOTO OBA <86038701+TokomaBaou@users.noreply.github.com>
2021-08-15 23:54:38 +0900

    state作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { React, useState } from "react";
 import "./styles.css";
 import { InputTodo } from "./components/InputTodo";
 import { IncompleteTodo } from "./components/IncompleteTodo";
@@ -6,9 +6,20 @@ import { CompleteTodo } from "./components/CompleteTodo";
 import { HabitList } from "./components/habitList";
 
 const App = () => {
+  const [todoText, setTodoText] = useState([""]);
+  const [incompleteTodos, setIncompleteTodos] = useState([]);
+  const [completeTodos, setCompleteTodos] = useState([]);
+  const [habitLists, setHabitLists] = useState([]);
+
+  const onClickAdd = () => {
+    const newTodos = [...incompleteTodos, todoText];
+    setIncompleteTodos(newTodos);
+    alert(todoText);
+  };
+
   return (
     <>
-      <InputTodo />
+      <InputTodo onClick={onClickAdd} />
       <div className="main-wrapper">
         <IncompleteTodo />
         <CompleteTodo />
