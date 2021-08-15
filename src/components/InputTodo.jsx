a2b8f46e2266f187d7909f93a80a85b72ef862de
MAKOTO OBA <86038701+TokomaBaou@users.noreply.github.com>
2021-08-15 23:36:52 +0900

    inputTodo

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 
+const style = {
+  // backgroundColor: "#c1ffff",
+  // borderRadius: "8px",
+  // width:"600px",
+  // textAlign: "center",
+  // alignItems: "center",
+};
+
 export const InputTodo = () => {
   return (
     <div className="top-wrapper">
       <p>本日のTODO</p>
-      <button>+</button>
-      <input placeholder="TODOを入力" />
+      <div style={style}>
+        <button>+</button>
+        <input placeholder="TODOを入力" />
+      </div>
     </div>
   );
 };
